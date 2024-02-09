150e370e359b2a99c1d8bc62974b91e51d6ebed0
Viviana Navarro <viviana.navarro1583@alumnos.udg.mx>
2024-02-09 10:38:45 -0600

    mi repositorio

---
+++ Presentacion.cpp
@@ -0,0 +1,9 @@
+    #include <stdio.h>
+
+    int main()
+    {
+        printf("Presentacion\n");
+        printf ("Nombre:Viviana Navarro Plascencia\nEdad:18\nResidencia:Tepatitlan de Morelos\n");
+        printf ("Fecha:30/01/2024\nCarrera:Ingenieria en computacion/2do semestre");
+        return 0;
+    }
