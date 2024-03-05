207f7dde18b3ed2e476f522ca8aa0913ec37ec0a
Viviana Navarro <150624359+Vivi-Navarro@users.noreply.github.com>
2024-03-04 23:15:17 -0600

    Add files via upload
    repositorio

---
+++ Actividad de programación - 9.cpp
@@ -0,0 +1,20 @@
+    /*Presentacíon
+    Viviana Navarro Plascencia
+    04/03/2024
+    Nombre: Actividad de programación - 9
+    Centro Universitario De los Altos
+    Ing en computacion/2do semestre
+    Profesor: Sergio Franco Casillas
+    Descripcion: Biblioteca de funciones*/
+    #include <iostream>
+    #include <vector>
+    #include <algorithm>
+    #include "codigocon funciones.cpp"
+    using namespace std;
+
+
+
+    int main() {
+        opcionesdemenu();
+        return 0;
+    }
